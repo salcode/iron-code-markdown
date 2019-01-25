25bce9d5e8ded387b3726e686217dafe2d5125c5
Sal Ferrarello <sal@ironcodestudio.com>
2019-01-25 09:44:53 -0500

    Modify MarkdownEdit to be a component

--- src/block/edit.js
+++ src/block/edit.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
 import { PlainText } from '@wordpress/editor';
 import { SandBox } from '@wordpress/components';
 
@@ -10,19 +11,24 @@ import { SandBox } from '@wordpress/components';
  */
 import marked from 'marked';
 
-export default function MarkdownEdit( { attributes, setAttributes, className, isSelected } ) {
-	if ( isSelected ) {
-		return (
-			<div className={ className }>
-				<PlainText
-					value={ attributes.content }
-					onChange={ ( content ) => setAttributes( { content } ) }
-					placeholder={ __( 'Write markdown…' ) }
-					aria-label={ __( 'Markdown' ) }
-				/>
-			</div>
-		);
-	}
+class MarkdownEdit extends Component {
+	render() {
+		const { attributes, setAttributes, className, isSelected } = this.props;
+		if ( isSelected ) {
+			return (
+				<div className={ className }>
+					<PlainText
+						value={ attributes.content }
+						onChange={ ( content ) => setAttributes( { content } ) }
+						placeholder={ __( 'Write markdown…' ) }
+						aria-label={ __( 'Markdown' ) }
+					/>
+				</div>
+			);
+		}
 
-	return <SandBox html={ marked( attributes.content || '' ) } />;
+		return <SandBox html={ marked( attributes.content || '' ) } />;
+	}
 }
+
+export default MarkdownEdit;
